3d4188d9d9a8343efb37c1433629594b633a55c3
MrM0ra <themonstermora@gmail.com>
2025-04-20 22:06:25 -0500

    adding new feature for testing. gitignore implemented

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -6,3 +6,10 @@ class InventoryPage(BasePage):
 
     def is_inventory_page_displayed(self):
         return self.find_element(self.TITLE).is_displayed()
+
+    def add_product_to_cart(self, product_name):
+        product_xpath = f"//div[text()='{product_name}']/ancestor::div[@class='inventory_item']//button"
+        self.driver.find_element(By.XPATH, product_xpath).click()
+
+    def get_cart_count(self):
+        return self.driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
